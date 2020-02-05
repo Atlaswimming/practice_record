b30c2b2bbd730ca9571e844c48101ac89562cf79
atlasmart <atlasmart@163.com>
2020-02-05 20:52:22 +0800

    add find square root of x

--- algorithm/binary_search/template1.js
+++ algorithm/binary_search/template1.js
@@ -23,9 +23,43 @@ const binarySearch = (nums=[], target) => {
     return -1;
 }
 
-console.log(binarySearch([-1,0,5,9,13], 9))
-console.log(binarySearch([-1,0,5,9,13], -1))
-console.log(binarySearch([-1,0,5,9,13], 13))
-console.log(binarySearch([-1,0,5,9,13], 6))
-console.log(binarySearch([5], 5))
-console.log(binarySearch([], 9))
+// console.log(binarySearch([-1,0,5,9,13], 9))
+// console.log(binarySearch([-1,0,5,9,13], -1))
+// console.log(binarySearch([-1,0,5,9,13], 13))
+// console.log(binarySearch([-1,0,5,9,13], 6))
+// console.log(binarySearch([5], 5))
+// console.log(binarySearch([], 9))
+
+
+const squareRoot = (x) => {
+    let start = 0;
+    let end = x;
+
+    while(start <= end) {
+        let bottom = Math.floor((start + end) / 2);
+        let top = bottom + 1;
+        let bottom_square = Math.pow(bottom, 2);
+        let top_square = Math.pow(top, 2);
+
+        if(bottom_square === x || (bottom_square < x && top_square > x)){
+            return bottom;
+        }
+        else if(top_square === x) {
+            return top;
+        }
+        else if(bottom_square > x) {
+            end = bottom
+        }
+        else {
+            start = top
+        }
+    }
+}
+
+console.log(squareRoot(0))
+console.log(squareRoot(1))
+console.log(squareRoot(2))
+console.log(squareRoot(4))
+console.log(squareRoot(5))
+console.log(squareRoot(8))
+console.log(squareRoot(100))
--- algorithm/binary_search/template1.py
+++ algorithm/binary_search/template1.py
@@ -18,9 +18,44 @@ def binary_search(nums, target):
 
     return -1
 
-print(binary_search([-1,0,5,9,13], 9))
-print(binary_search([-1,0,5,9,13], -1))
-print(binary_search([-1,0,5,9,13], 13))
-print(binary_search([-1,0,5,9,13], 6))
-print(binary_search([5], 5))
-print(binary_search([], 9))
+# print(binary_search([-1,0,5,9,13], 9))
+# print(binary_search([-1,0,5,9,13], -1))
+# print(binary_search([-1,0,5,9,13], 13))
+# print(binary_search([-1,0,5,9,13], 6))
+# print(binary_search([5], 5))
+# print(binary_search([], 9))
+
+class SquareRoot:
+    """
+    Compute and return the square root of x,
+    where x is guarranteed to be a  non-negative integer
+    """
+    def mySqrt(self, x: int) -> int:
+        start = 0
+        end = x
+        while start <= end:
+            # << 按位左移，左移n位相当于乘以2的n次方
+            # >> 按位右移，左移n位相当于除以2的n次方
+            bottom = (start + end) >> 1
+            top = bottom + 1
+
+            bottom_square = bottom**2
+            top_value = top**2
+            
+            if bottom_square == x or (bottom_square <= x and top_value > x):
+                return bottom
+            elif top_value == x:
+                return top
+            elif bottom_square > x:
+                end = bottom
+            else:
+                start = top
+
+square_root = SquareRoot()
+print(square_root.mySqrt(0))
+print(square_root.mySqrt(1))
+print(square_root.mySqrt(2))
+print(square_root.mySqrt(4))
+print(square_root.mySqrt(5))
+print(square_root.mySqrt(8))
+print(square_root.mySqrt(100))
